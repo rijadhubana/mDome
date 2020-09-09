23e1c59ce16eb46348b1b810930f436de1efca86
Rijad Hubana <rijad.hubana@edu.fit.ba>
2020-09-10 00:40:20 +0200

    deleted commented lines

--- mDome.MobileApp/mDome.MobileApp/Views/MasterView.xaml.cs
+++ mDome.MobileApp/mDome.MobileApp/Views/MasterView.xaml.cs
@@ -22,43 +22,6 @@ namespace mDome.MobileApp.Views
         }
         MasterViewModel vm  = null;
         public event EventHandler<object> masterViewEventHandler;
-        //private void MenuItemTapped(object sender, EventArgs e)
-        //{
-        //    //if (vm.SelectedMenuItem == "News Feed")
-        //    //    Application.Current.MainPage = new MasterPage(new NavigationPage(new NewsFeedPage()));
-        //    //else if (vm.SelectedMenuItem == "My Profile")
-        //    //    Application.Current.MainPage = new MasterPage(new NavigationPage(new UserProfilePage(APIService.loggedProfile.UserId)));
-        //    //else if (vm.SelectedMenuItem== "Followers/Following")
-        //    //    Application.Current.MainPage = new MasterPage(new NavigationPage(new FollowersFollowingPage()));
-        //    //else if (vm.SelectedMenuItem=="Notifications")
-        //    //    Application.Current.MainPage = new MasterPage(new NavigationPage(new NotificationListPage()));
-        //    //else if (vm.SelectedMenuItem == "Search")
-        //    //    Application.Current.MainPage = new MasterPage(new NavigationPage(new SearchPage()));
-        //    //else if (vm.SelectedMenuItem == "My Playlists/Album Lists")
-        //    //     Application.Current.MainPage= new MasterPage(new NavigationPage(new MyTracklistsAlbumlistsPage()));
-        //    //else if (vm.SelectedMenuItem == "History")
-        //    //{
-        //    //    APIService _tracklistService = new APIService("Tracklist");
-        //    //    var history = await _tracklistService.Get<List<Tracklist>>(new TracklistSearchRequest()
-        //    //    {
-        //    //        UserId = APIService.loggedProfile.UserId,
-        //    //        TracklistName = "History"
-        //    //    });
-        //    //    if (history.Count!=0)
-        //    //        Application.Current.MainPage = new MasterPage(new NavigationPage(new TracklistPage(history.First().TracklistId)));
-        //    //}
-        //    //else if(vm.SelectedMenuItem== "Report a Problem")
-        //    //    Application.Current.MainPage = new MasterPage(new NavigationPage(new ReportAProblemPage()));
-        //    //else if (vm.SelectedMenuItem == "Logout")
-        //    //{
-        //    //    APIService.loggedProfile = null;
-        //    //    APIService.Username = "";
-        //    //    APIService.Password = "";
-        //    //    Application.Current.MainPage = new LoginPage();
-        //    //}
-        //    //vm.SelectedMenuItem = "";
-        //}
-
         private void MenuItemTapped(object sender, SelectedItemChangedEventArgs e)
         {
             masterViewEventHandler?.Invoke(this, vm.SelectedMenuItem);
